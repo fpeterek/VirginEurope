3fe54c61b5bb5df8824acb1622f091c5bb6d0bc5
Filip Peterek <fpeterek@seznam.cz>
2020-04-13 19:10:55 +0200

    Progress on Insert statement

--- src/main/java/org/fpeterek/virgineurope/orm/sql/Insert.java
+++ src/main/java/org/fpeterek/virgineurope/orm/sql/Insert.java
@@ -1,10 +1,43 @@
 package org.fpeterek.virgineurope.orm.sql;
 
+import org.fpeterek.virgineurope.orm.Attribute;
 import org.fpeterek.virgineurope.orm.Table;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Insert extends DMLQuery {
 
+  private static class AttributeList {
+
+    private final List<Attribute> attrs;
+    private final Insert origInsert;
+
+    public AttributeList(List<Attribute> attributes, Insert insert) {
+      attrs = attributes;
+      origInsert = insert;
+    }
+
+    public Insert values(String... args) {
+
+      if (args.length != attrs.size()) {
+        throw new IllegalArgumentException(
+          "Number of values supplied does not match number of arguments specified."
+        );
+      }
+
+      origInsert.values = Arrays.asList(args);
+      origInsert.attributes = attrs;
+
+      return origInsert;
+    }
+
+  }
+
   private final Table intoTable;
+  private List<Attribute> attributes;
+  private List<String> values;
 
   private Insert(Table table) {
     intoTable = table;
@@ -12,6 +45,10 @@ public class Insert extends DMLQuery {
 
   public static Insert into(Table table) {
     return new Insert(table);
+  }
+
+  public AttributeList attributes(Attribute... attrs) {
+    return new AttributeList(Arrays.asList(attrs), this);
   }
 
   @Override
