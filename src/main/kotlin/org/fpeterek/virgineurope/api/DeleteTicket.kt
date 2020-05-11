e80ebcbc03bed327370bb30cdd1867d8a7f080b5
Filip Peterek <fpeterek@seznam.cz>
2020-05-11 12:01:38 +0200

    Old tickets cannot be refunded

--- src/main/kotlin/org/fpeterek/virgineurope/api/DeleteTicket.kt
+++ src/main/kotlin/org/fpeterek/virgineurope/api/DeleteTicket.kt
@@ -1,6 +1,8 @@
 package org.fpeterek.virgineurope.api
 
 import org.fpeterek.virgineurope.orm.VU
+import org.joda.time.LocalDate
+import org.joda.time.LocalTime
 
 object DeleteTicket {
 
@@ -8,10 +10,34 @@ object DeleteTicket {
         DELETE FROM VU.flightTicket WHERE (VU.flightTicket.ticketId EQ id)
     )
 
+    private fun canBeDeleted(id: Int): Boolean {
+        val ticket = DB.execute(
+            SELECT FROM VU.flightTicket JOIN VU.operatedFlight ON
+                    (VU.operatedFlight.id EQ VU.flightTicket.operatedId) JOIN
+                    VU.flight ON (VU.flight.id EQ VU.operatedFlight.flightId) WHERE
+                    (VU.flightTicket.ticketId EQ id)
+        ).flightTickets.firstOrNull()
+
+        ticket ?: return true
+
+        val dateNow = LocalDate.now()
+        val timeNow = LocalTime.now()
+        val ticketDate = ticket.operatedFlight.date
+        val ticketTime = ticket.operatedFlight.flight.departureTime
+
+        return ticketDate > dateNow || (ticketDate == dateNow && timeNow < ticketTime)
+
+    }
+
     fun delete(ticket: String): String {
         val id = ticket.toIntOrNull() ?: return """{"error": "Invalid ID"}"""
-        deleteTicket(id)
-        return """{"success": "success"}"""
+
+        return if (canBeDeleted(id)) {
+            deleteTicket(id)
+            """{"success": "success"}"""
+        } else {
+            """{"error": "Old tickets cannot be refunded"}"""
+        }
     }
 
 }
